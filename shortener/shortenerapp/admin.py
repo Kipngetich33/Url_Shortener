80b49a375e23ef891409f3bfb4a158a8c3f8a027
Kipngetich33 <khalifngeno@gmail.com>
2018-02-13 11:12:07 +0300

    create the first model URLS and migrate

--- shortener/shortenerapp/admin.py
+++ shortener/shortenerapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from shortenerapp.models import Urls
 
-# Register your models here.
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = ('short_id','httpurl','pu')
