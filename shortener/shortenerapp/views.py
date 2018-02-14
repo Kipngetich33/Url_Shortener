d216393411e7cddc93cc22e213875e02f6b3b4b1
Kipngetich33 <khalifngeno@gmail.com>
2018-02-14 07:50:49 +0300

    update the makeshort view function to update only when a new url is added

--- shortener/shortenerapp/views.py
+++ shortener/shortenerapp/views.py
@@ -10,11 +10,20 @@ def r(request):
     if 'name' in request.GET and request.GET["name"]: 
         search_name = request.GET.get("name")
         shortcode = code_generator(6,'1234567890afuhufxrkerwcklbvds')
-        prefix = 'http//:'
-        short_url = prefix + shortcode
+        prefix = 'http//:r/'
 
-        return render(request,'makeshort.html',{"short_url":short_url})
-    return render(request,'makeshort.html')
+        try:
+            is_saved = Urls.objects.get( httpurl = search_name)
+            short_url = prefix+is_saved.short_id
+            message = 'Url already exists'
+            return render(request,'makeshort.html',{"short_url":short_url, "message":message})
+        except:
+            short_url = prefix + shortcode
+            new_id = Urls(short_id = shortcode,httpurl = search_name)
+            new_id.count +=1
+            new_id.save()
+            message = 'Short Url Created successfully'
+            return render(request,'makeshort.html',{"short_url":short_url, "message":message})
 
 def s(request):
     return render(request,'statistics.html')
