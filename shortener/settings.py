7577548dc719372b7e2dcf53662e038bf490143f
Kipngetich33 <khalifngeno@gmail.com>
2018-02-14 18:49:28 +0300

    add changes to settings to push to heroku

--- shortener/settings.py
+++ shortener/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
         'PASSWORD': 'Empharse333',
     }
 }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env) 
@@ -126,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
 STATICFILES_STORAGE ='whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
