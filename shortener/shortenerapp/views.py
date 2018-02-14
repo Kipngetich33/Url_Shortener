73a61e3bc2705af87a0b97b9fafb40e67aea568d
Kipngetich33 <khalifngeno@gmail.com>
2018-02-14 13:09:31 +0300

    create a path all the way the the s view function

--- shortener/shortenerapp/views.py
+++ shortener/shortenerapp/views.py
@@ -9,26 +9,27 @@ def h(request):
 def r(request):
     if 'name' in request.GET and request.GET["name"]: 
         search_name = request.GET.get("name")
-        shortcode = code_generator(6,'1234567890afuhufxrkerwcklbvds')
         prefix = 'http//:r/'
         httpurl = prefix
 
         count = Urls.objects.filter(httpurl = search_name).count()
         if count <= 0:
-            message = 'inexistent'
+            shortcode = code_generator(6,'1234567890afuhufxrkerwcklbvds')
+            new_url = Urls (short_id = shortcode,httpurl = search_name )
+            new_url.save()
+            httpurl = search_name 
+            message = 'short code created successfully'
         else:
-            message = 'Exitent'
+            shortcode = Urls.objects.get(httpurl = search_name).short_id
+            httpurl = Urls.objects.get(httpurl = search_name).httpurl
+            message = 'A short url for the entered url already exists'
     else:
-        message = 'No input'
+        shortcode ='You cannot create a shortcode with an empty input'
+        message = 'You have not entered any url'
+        httpurl = ''
 
-    return render(request,'makeshort.html',{"message":message})
+    return render(request,'makeshort.html',{"message":message,"shortcode":shortcode, "httpurl":httpurl})
 
-def s(request, httpurl):
-    try:    
-        is_saved = Urls.objects.get( httpurl = httpurl)
-        short_url = is_saved.short_id
-        message = 'Url already exists'
-        return render(request,'makeshort.html',{"short_url":short_url, "message":message})
-    except:
-        message = 'Url already exists'
-        return render(request,'statistics.html',{"httpurl":httpurl,"message":message})
+def s(request, httpurl): 
+    httpurl = httpurl
+    return redirect(f'{{httpurl}}')
